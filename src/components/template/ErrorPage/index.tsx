4a21ad418b8720a7e57c24283c34d23ab716b953
uuronn <uuronprogram@gmail.com>
2022-03-17 19:31:46 +0900

    first commit

---
+++ src/components/template/ErrorPage/index.tsx
@@ -0,0 +1,1 @@
+export const ErrorPageTemplate = (): JSX.Element => <h1>404 error page</h1>
---
+++ src/pages/404.tsx
@@ -0,0 +1,6 @@
+import type { NextPage } from 'next'
+import { ErrorPageTemplate } from '../components/template/ErrorPage'
+
+const Home: NextPage = () => <ErrorPageTemplate/>
+
+export default Home
