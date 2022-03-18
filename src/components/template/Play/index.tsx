bfe7f1e22cf1b7b244d7a61884734380c2958287
uuronn <uuronprogram@gmail.com>
2022-03-18 19:59:43 +0900

    文字が合っていたらalert

--- src/components/template/Play/index.tsx
+++ src/components/template/Play/index.tsx
@@ -3,48 +3,54 @@ import { monster } from './monster'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
-const input = css`
+const typeText = css`
+  font-size: 20px;
+  padding: 20px;
   border: solid 1px #000;
-`
+  width: 500px;
+`;
 
 export const PlayTemplate = (): JSX.Element => {
   const [value, setValue] = useState<string>("")
-  const [keyChara, setKeyChara] = useState<string>("")
-
+  console.log(monster[0].romaji.toLowerCase())
+  
+  console.log(value.toLowerCase())
   const keyDown = (e) => {
-    const keyCode = e.code;
-    const key = e.key;
-    console.log(key)
-    console.log(keyCode)
-    setValue(prevState => prevState + key)
-    console.log(value)
-    // let keyDown: HTMLElement | null = document.getElementById('keyDown');
-    // const myCode = String.fromCharCode(keyCode);
-    // console.log(myCode)
-    // let myCodeLower = myCode.toLowerCase();
-    // setValue(value += myCodeLower)
-    // keyDown.innerHTML += myCodeLower;
-  }
+    if(e.code === "Backspace") {
+      // console.log("delete")
+      setValue(prevState => prevState + key)
+      // str.indexOf( 検索したい文字, 検索開始位置 );
+      return
+    }
 
-  const keyChange = (e) => {
+    // if(e.code === value.indexOf(monster[0].romaji.indexOf()))
+    
+    const code = e.code;
+    const key = e.key;
+    // console.log(code)
+    // console.log(key)
+    setValue(prevState => prevState + key)
   }
 
   useEffect(() => {
     window.addEventListener('keydown', keyDown);
-    // window.addEventListener('keydown', keyChange);
   }, [])
+
+  useEffect(() => {
+    // console.log(value.length)
+    if(monster[0].romaji === value) {
+      alert("sucsees")
+      setValue("")
+    }
+  }, [value])
 
   return (
     <div>
       <p>タイピングページ</p>
       <Image src={monster[0].imagePath} alt="プリン" width={200} height={200} />
+      <p>{monster[0].japanese}</p>
       <p>{monster[0].romaji}</p>
-      <input
-        css={input}
-        type='text'
-        value={value}
-        onChange={keyChange}
-      />
+      <p css={typeText}>タイプ文字: {value}</p>
     </div>
   )
 }
