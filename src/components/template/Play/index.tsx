c28da5c9e98e1485013ec8b0a2c2a609ef35d255
uuronn <uuronprogram@gmail.com>
2022-03-18 17:18:29 +0900

    inputと取得したkeyをバインド

--- src/components/template/Play/index.tsx
+++ src/components/template/Play/index.tsx
@@ -1,33 +1,50 @@
 import { css } from '@emotion/react'
 import { monster } from './monster'
 import Image from 'next/image'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const input = css`
-  border: solid 2px #000;
+  border: solid 1px #000;
 `
 
-
 export const PlayTemplate = (): JSX.Element => {
-  useEffect(() => {
-    addEventListener('keydown', keyPress);
-  }, [])
-  const keyPress = (e) => {
+  const [value, setValue] = useState<string>("")
+  const [keyChara, setKeyChara] = useState<string>("")
+
+  const keyDown = (e) => {
     const keyCode = e.code;
+    const key = e.key;
+    console.log(key)
     console.log(keyCode)
-    let keyDown: HTMLElement | null = document.getElementById('keyDown');
-    const myCode = String.fromCharCode(keyCode);
-    let myCodeLower = myCode.toLowerCase();
-    keyDown.innerHTML += myCodeLower;
+    setValue(prevState => prevState + key)
+    console.log(value)
+    // let keyDown: HTMLElement | null = document.getElementById('keyDown');
+    // const myCode = String.fromCharCode(keyCode);
+    // console.log(myCode)
+    // let myCodeLower = myCode.toLowerCase();
+    // setValue(value += myCodeLower)
+    // keyDown.innerHTML += myCodeLower;
   }
+
+  const keyChange = (e) => {
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyDown);
+    // window.addEventListener('keydown', keyChange);
+  }, [])
 
   return (
     <div>
-      <p>あそぶページ</p>
+      <p>タイピングページ</p>
       <Image src={monster[0].imagePath} alt="プリン" width={200} height={200} />
       <p>{monster[0].romaji}</p>
-      <div id="keyDown"></div>
-      <input css={input} type='text' onKeyPress={(e) => keyPress(e)} />
+      <input
+        css={input}
+        type='text'
+        value={value}
+        onChange={keyChange}
+      />
     </div>
   )
 }
