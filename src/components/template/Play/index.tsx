720126dc81b572930f29c97c74f1994944bd5757
uuronn <uuronprogram@gmail.com>
2022-03-21 00:43:04 +0900

    タイプミスをカウント

--- src/components/template/Play/index.tsx
+++ src/components/template/Play/index.tsx
@@ -9,33 +9,27 @@ const typeText = css`
   border: solid 1px #000;
   width: 500px;
 `;
-
+  
 export const PlayTemplate = (): JSX.Element => {
   const [value, setValue] = useState<string>("")
   const [monsterList, setMonsterList] = useState(monster)
-  console.log(...monsterList)
+  const [missCounter, setmissCounter] = useState(0)
+  let question = monster[0].romaji.split("")
 
-  // is.questions.splice(Math.floor(Math.random()*this.questions.length),1)[0]
-
-  console.log(monster[0].romaji.toLowerCase())
-  
-  console.log(value.toLowerCase())
-  const keyDown = (e) => {
-    if(e.code === "Backspace") {
-      // console.log("delete")
-      setValue(prevState => prevState + key)
-      // str.indexOf( 検索したい文字, 検索開始位置 );
-      return
-    }
-
-    // if(e.code === value.indexOf(monster[0].romaji.indexOf()))
-    
-    
+  const keyDown = (e: any) => {
     const code = e.code;
     const key = e.key;
-    // console.log(code)
-    // console.log(key)
-    setValue(prevState => prevState + key)
+
+    if(key !== question[0]) {
+      setmissCounter(count => count + 1)
+      return;
+    }
+
+    if(key === question[0]) {
+      question.splice(0, 1)
+    }
+
+    setValue(val => val + key)
   }
 
   useEffect(() => {
@@ -43,8 +37,7 @@ export const PlayTemplate = (): JSX.Element => {
   }, [])
 
   useEffect(() => {
-    // console.log(value.length)
-    if(monster[0].romaji === value) {
+    if(monsterList[0].romaji === value) {
       alert("sucsees")
       setValue("")
     }
@@ -52,13 +45,15 @@ export const PlayTemplate = (): JSX.Element => {
 
   return (
     <div>
-      <p>タイピングページ</p>
-      <Image src={monster[0].imagePath} alt="プリン" width={200} height={200} />
-      <p>{monster[0].japanese}</p>
-      <p>{monster[0].romaji}</p>
+      <h1>タイピングページ</h1>
+      {/* <Image src={monster[0].imagePath} alt="プリン" width={200} height={200} />
+      <p>{monster[0].japanese}</p> */}
+      {/* <p>{monster[0].romaji}</p> */}
+      <p>{missCounter}</p>
+      <p>aa:{question}</p>
       <p css={typeText}>タイプ文字: {value}</p>
       <ul>
-        {monster.map((monsterItem, i) => {
+        {/* {monsterList.map((monsterItem, i) => {
           return (
             <li key={i}>
               <Image src={monsterItem.imagePath} alt={monsterItem.japanese} width={200} height={200} />
@@ -67,7 +62,7 @@ export const PlayTemplate = (): JSX.Element => {
             </li>
             // <MindCard mindItem={mindItem} key={i} keyProps={i} updateRect={updateRect}/>
           )
-        })}
+        })} */}
       </ul>
     </div>
   )
