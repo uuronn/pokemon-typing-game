d9c74b8ba0c30fd73c90456f26165a6d656ba3b6
uuronn <uuronprogram@gmail.com>
2022-06-02 18:15:49 +0900

    ユーザごとのデータ管理

--- src/components/domain/auth/LoginContent/index.tsx
+++ src/components/domain/auth/LoginContent/index.tsx
@@ -6,7 +6,8 @@ import {
   onAuthStateChanged
 } from "firebase/auth";
 import { useEffect, useState } from "react";
-import { auth } from "~/src/infra/firebase";
+import { auth, db } from "~/src/infra/firebase";
+import { doc, setDoc } from "firebase/firestore";
 
 export const LoginContent = ({ ...props }): JSX.Element => {
   const [uid, setUid] = useState<string>("");
@@ -14,14 +15,18 @@ export const LoginContent = ({ ...props }): JSX.Element => {
   const provider = new GoogleAuthProvider();
 
   const signIn = async () => {
-    try {
-      const res = await signInWithPopup(auth, provider);
+    if (!uid) {
+      try {
+        const res = await signInWithPopup(auth, provider);
 
-      if (!res) throw new Error("res error");
+        if (!res) throw new Error("res error");
 
-      setUid(res.user.uid);
-    } catch (error) {
-      console.error(error);
+        setUid(res.user.uid);
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      alert("ログイン中です");
     }
   };
 
@@ -38,12 +43,19 @@ export const LoginContent = ({ ...props }): JSX.Element => {
 
   useEffect(() => {
     if (uid) {
-      console.log(uid);
+      const uidFun = async () => {
+        await setDoc(doc(db, "users", uid), {
+          score: 0,
+          mode: "default"
+        });
+      };
+      uidFun();
     }
   }, [uid]);
 
   return (
     <div {...props}>
+      <p>自己ベスト: </p>
       <button onClick={signIn} css={button}>
         <GoogleIcon />
         ログイン
